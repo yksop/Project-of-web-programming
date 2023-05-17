fb91db37fd73d9c7df5220ac5d15905f6e8b3a7f
Jacopo <119100169+JacopoScalco02@users.noreply.github.com>
2023-05-17 20:35:32 +0200

    Login/Registrazione+citazioni
    file css per form di login/registrazione, file css per citazioni, file .js per business logic del form registrazione e citazioni con ajax. modifiche al template con inserimento di "div" per citazioni.

---
+++ src/main/webapp/JavaScript/changeCitations.js
@@ -0,0 +1,25 @@
+
+
+// Funzione per ottenere una citazione casuale dal file JSON
+function getCitazioneCasuale() {
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            var data = JSON.parse(xhr.responseText);
+            var citazioni = data.citazioni;
+            var indice = Math.floor(Math.random() * citazioni.length);
+            var citazione = citazioni[indice];
+            var citazioneDiv = document.getElementById("citazione");
+            citazioneDiv.innerHTML = citazione;
+        }
+    };
+    xhr.open("GET", "DataJSON/citazioni.json", true);
+    xhr.send();
+}
+
+// Aggiorna la citazione iniziale
+getCitazioneCasuale();
+
+// Aggiorna la citazione ogni 20 secondi
+setInterval(getCitazioneCasuale, 20000);
+
