353c105989142ff3285925b727999cbd8a9a2505
Daniele Notarangelo <50075274+danielenotarangelo@users.noreply.github.com>
2023-06-08 10:01:33 +0200

    Add files via upload

---
+++ src/main/java/Controllers/InvioEmail.java
@@ -0,0 +1,76 @@
+package Controllers;
+
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Properties;
+
+@WebServlet(name = "InvioEmail", value = "/InvioEmail")
+public class InvioEmail extends HttpServlet {
+    String nome, cognome, email, messaggio, oggetto;
+    public void invio(HttpServletRequest request, HttpServletResponse response) {
+        nome = request.getParameter("nome");
+        cognome = request.getParameter("cognome");
+        email = request.getParameter("email");
+        oggetto = request.getParameter("motivo");
+        messaggio = request.getParameter("dettagli");
+
+        String email_ricevente = "tum4world@nessunonoluogonoesiste.com";
+        String host = "smtp.nessunonoluogonoesiste.com";
+        String username = email;
+        String password = "admin";
+
+        Properties properties = new Properties();
+        properties.put("mail.smtp.auth", true);
+        properties.put("mail.smtp.starttls.enable", true);
+        properties.put("mail.smtp.host", host);
+        properties.put("mail.smtp.port", 587);
+
+        Session session = Session.getInstance(properties, new Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(username, password);
+            }
+        });
+
+        try {
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(username));
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(email_ricevente));
+            message.setSubject(oggetto);
+            message.setText(nome + cognome + messaggio);
+
+            RequestDispatcher dispatcher = request.getRequestDispatcher("/invioConfermato");
+
+            if(dispatcher != null) {
+                dispatcher.forward(request, response);
+            } else {
+                System.out.println("pagina non trovata");
+            }
+
+            Transport.send(message);
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        } catch (ServletException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        invio(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        invio(request, response);
+    }
+}
